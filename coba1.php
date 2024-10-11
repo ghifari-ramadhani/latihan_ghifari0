dad5cb5763f4851a7f8d0e94182e66da7d6c1670
ghifari-ramadhani <gifari230@.com>
2024-10-11 14:40:32 +0700

    membuat form 1

--- coba1.php
+++ coba1.php
@@ -7,6 +7,19 @@
     <title>Document</title>
 </head>
 <body>
-    <p>aku mau mam emimh</p>
+    <!-- tag pembuka form -->
+<!-- action dan method merupakan atribut, psot merupakan value-->
+<!-- post tidak di tampilkan di url,sedangkan get di tampilkan -->
+    <form action="form_action.php" method="post">
+        <label for="">Username :</label>
+        <!-- name adalah variabel, username adalah value/nilai -->
+        <input type="text" name="username"
+        placeholder="Username">
+
+        <button type="submit">Kirim</button>
+    </form>
+    <!-- tag penutup form -->
+    <br><br><br>
+    
 </body>
 </html>
