aa2530dcbcbd5bd28f362a6d67c8858f74435c09
ghifari-ramadhani <gifari230@.com>
2024-10-11 14:38:13 +0700

    membuat form

---
+++ coba3.php
@@ -0,0 +1,7 @@
+<?php
+//menampung nilai yg di input oleh username
+$usename = $_POST['username'];
+//menampilkan value/nilai yg di input kan oleh user
+echo "Username nya adalah " . $usename;
+
+?>
